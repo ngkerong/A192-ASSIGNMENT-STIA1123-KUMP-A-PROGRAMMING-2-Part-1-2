5a5af5ca5774787d33476bfb11e54cfa05b96cac
ngkerong <55265526+ngkerong@users.noreply.github.com>
2020-06-01 11:28:56 +0800

    Add files via upload

---
+++ Part 1/Internet.java
@@ -0,0 +1,81 @@
+/*Name:Ng Ke Rong*/
+/*No.Matrik:271063*/
+
+import java.util.*;
+
+public class Internet{
+
+   public static void main(String[] args){
+   
+        Scanner input = new Scanner(System.in);
+        tmStreamyx wifi1 = new tmStreamyx();
+        tmUnifi wifi2 = new tmUnifi();
+        Time wifi3 = new Time();
+        Celcom wifi4 = new Celcom();
+        
+        String name, telNo, address;
+        int category, respond;
+        
+        System.out.println("\t\t\t\tWelcome to Internet Serving System\n");
+        
+        System.out.printf("Name :");
+        name = input.nextLine();
+        
+        System.out.printf("Tel.No :");
+        telNo = input.nextLine();
+        
+        System.out.printf("Address :");
+        address = input.nextLine();
+
+        do{    
+            
+            System.out.println("\n1.\tTM Streamyx");
+            System.out.println("2.\tTM Unifi");
+            System.out.println("3.\tTime");
+            System.out.println("4.\tCelcom");
+            
+            
+            do{
+                System.out.printf("Select the type of internet you want:");
+                category = input.nextInt();
+            }while( category != 1 & category != 2 & category != 3 & category != 4);
+        
+            System.out.println("Name :" + name); 
+            System.out.println("Tel.No :" + telNo);
+            System.out.println("Address :"+ address);
+            
+            switch (category) {
+                case 1:
+                    
+                    System.out.println(wifi1.display() + wifi1.special("Wired"));
+                    
+                    break;
+                case 2:
+                    
+                    System.out.println(wifi2.display() + wifi2.special("Wired"));
+                    
+                    break;
+                case 3:
+                
+                    System.out.println(wifi3.display() + wifi3.special("Wireless"));
+                    
+                    break;
+                default:
+                    
+                    System.out.println(wifi4.display() + wifi4.special("Wireless"));
+                    
+                    break;
+            }
+            
+            
+            do{
+                System.out.println("\nIs this correct? 1.Yes  2.No\t: ");
+                respond = input.nextInt();
+            }while(respond != 1 & respond != 2);
+        
+        }while(respond == 2);    
+        
+        System.out.println("Thank you, have a good day.");
+    }   
+    
+}
